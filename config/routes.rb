0dc2cacd9c52b59d328e4c6b89437e59a48f9bf6
Daniel Harris <bobth3bum@gmail.com>
2015-02-18 10:43:54 -0700

    add routes for sessions and users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :users
+
   # You can have the root of your site routed with "root"
   root 'users#login'
+
+  get 'login' => 'sessions#new', :as => 'login'
+  post 'login' => 'sesions#create'
+  get 'logout' => 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
