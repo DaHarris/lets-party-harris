7e227a8cb2dac02814f2a971031247954b012567
Daniel Harris <bobth3bum@gmail.com>
2015-02-21 15:54:42 -0700

    added map and current location to default

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,53 +21,38 @@ $(document).foundation();
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see a blank space instead of the map, this
 // is probably because you have denied permission for location sharing.
+function go() {
 
-var map;
+    if(!!navigator.geolocation) {
 
-function initialize() {
-  var mapOptions = {
-    zoom: 6
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+        var map;
 
-  // Try HTML5 geolocation
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
+        var mapOptions = {
+            zoom: 13,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
 
-      var infowindow = new google.maps.InfoWindow({
-        map: map,
-        position: pos,
-        content: 'Location found using HTML5.'
-      });
+        map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-      map.setCenter(pos);
-    }, function() {
-      handleNoGeolocation(true);
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleNoGeolocation(false);
-  }
+        navigator.geolocation.getCurrentPosition(function(position) {
+
+            var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            var infowindow = new google.maps.InfoWindow({
+                map: map,
+                position: geolocate,
+                content:
+                    '<p>Your Location!</p>' +
+                    '<p>Latitude: ' + position.coords.latitude + '</p>' +
+                    '<p>Longitude: ' + position.coords.longitude + '</p>'
+            });
+
+            map.setCenter(geolocate);
+
+        });
+
+    } else {
+        document.getElementById("map-canvas").innerHTML = 'No Geolocation Support.';
+    }
+
 }
-
-function handleNoGeolocation(errorFlag) {
-  if (errorFlag) {
-    var content = 'Error: The Geolocation service failed.';
-  } else {
-    var content = 'Error: Your browser doesn\'t support geolocation.';
-  }
-
-  var options = {
-    map: map,
-    position: new google.maps.LatLng(60, 105),
-    content: content
-  };
-
-  var infowindow = new google.maps.InfoWindow(options);
-  map.setCenter(options.position);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
